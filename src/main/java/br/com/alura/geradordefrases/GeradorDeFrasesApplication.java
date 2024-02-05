5b3c009de933fe339536270718392723905ac349
Tiago Bastos <tbbgoncalves@outlook.com>
2024-02-05 16:12:34 -0300

    commit inicial

---
+++ src/main/java/br/com/alura/geradordefrases/GeradorDeFrasesApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.geradordefrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorDeFrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorDeFrasesApplication.class, args);
+	}
+
+}
